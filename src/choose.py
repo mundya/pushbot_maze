6add7228b40f25c1d46f9a20b8b6299b43beedd8
Terry Stewart <tcstewar@uwaterloo.ca>
2014-10-19 17:03:58 -0400

    random choice

--- src/choose.py
+++ src/choose.py
@@ -1,4 +1,5 @@
 import nengo
+import nengo.utils.functions
 
 model = nengo.Network()
 with model:
@@ -33,13 +34,30 @@ with model:
         #print 'state', ' '.join(['%1.2f' % xx for xx in x])
     nengo.Connection(state, nengo.Node(print_state, size_in=3), synapse=0.01)
     def print_actions(t, x):
-        print 'action', ' '.join(['%1.2f' % xx for xx in x])
+        pass
+        #print 'action', ' '.join(['%1.2f' % xx for xx in x])
     nengo.Connection(actions.output, nengo.Node(print_actions, size_in=2),
             synapse=0.01)
 
     nengo.Connection(actions.ensembles[0], actions.ensembles[1], transform=-5)
     nengo.Connection(actions.ensembles[1], actions.ensembles[0], transform=-5)
 
+    noise = nengo.Node(nengo.utils.functions.whitenoise(0.1, 10, rms=0.05,
+                                                       dimensions=2))
+    nengo.Connection(noise, actions.input)
+
+    pAction = nengo.Probe(actions.output, synapse=0.03)
+    pState = nengo.Probe(state, synapse=0.03)
+
 
 sim = nengo.Simulator(model)
-sim.run(1)
+sim.run(5)
+
+import pylab
+pylab.subplot(2,1,1)
+pylab.plot(sim.trange(), sim.data[pState])
+
+pylab.subplot(2,1,2)
+pylab.plot(sim.trange(), sim.data[pAction])
+
+pylab.show()
