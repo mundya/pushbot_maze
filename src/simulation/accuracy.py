a5c10c9c8f45c8bc2417ff1ea2c590f5532a3f93
Terry Stewart <tcstewar@uwaterloo.ca>
2015-11-26 15:08:06 -0500

    Made example figures for simulation of model

--- src/simulation/accuracy.py
+++ src/simulation/accuracy.py
@@ -10,6 +10,7 @@ class AccuracySim(ctn_benchmark.Benchmark):
         self.default('synapse', synapse=0.01)
         self.default('stimulus type', stim='ramp')
         self.default('number of training examples', n_training=1)
+        self.default('number of bumps', n_bumps=2)
         self.default('signal strength', strength=1.0)
 
     def model(self, p):
@@ -21,10 +22,18 @@ class AccuracySim(ctn_benchmark.Benchmark):
         train_response = np.zeros((N, 1))
         width = 0.2
         train_response[:,0] += np.exp(-(t-p.T*3/4)**2/(2*width**2))
+        if p.n_bumps > 1:
+            train_response[:,0] += np.exp(-(t-p.T*1/4)**2/(2*width**2))
+
 
         if p.stim == 'ramp':
             train_stim = np.zeros((N, p.D))
-            train_stim[:,0] += np.linspace(-1, 1, N)
+            if p.n_bumps == 1:
+                train_stim[:,0] += np.linspace(-1, 1, N)
+            else:
+                train_stim[:N/2,0] += np.linspace(-1, 1, N/2)
+                train_stim[N/2:,0] += np.linspace(-1, 1, N/2)
+
 
             test_sim = train_stim
             test_response = train_response
@@ -84,7 +93,12 @@ class AccuracySim(ctn_benchmark.Benchmark):
             plt.subplot(2, 1, 2)
             plt.plot(sim.trange(), sim.data[self.probe_result], label='result')
             plt.plot(sim.trange(), sim.data[self.probe_ideal], lw=2, label='ideal')
-            plt.legend(loc='best')
+            plt.legend(loc='upper left')
+            ylabel = 'action strength\nwith %d training example%s' % (
+                            p.n_training, 's' if p.n_training>1 else '')
+            plt.ylabel(ylabel)
+            plt.ylim(-0.2, 1.2)
+            plt.xlabel('time (s)')
 
         rmse_train = sim.data[self.conn].solver_info['rmses']
         diff = sim.data[self.probe_result] - sim.data[self.probe_ideal]
@@ -93,6 +107,8 @@ class AccuracySim(ctn_benchmark.Benchmark):
         dp = np.dot(sim.data[self.probe_result][:,0], sim.data[self.probe_ideal][:,0])
         dp = dp / np.linalg.norm(sim.data[self.probe_result])
         dp = dp / np.linalg.norm(sim.data[self.probe_ideal])
+        if plt:
+            plt.text(p.T/2, 0.9, 'Similarity: %1.3f' % dp, ha='center')
 
         return dict(rmse_train=rmse_train[0], rmse_test=rmse_test, dp_norm=dp)
 
